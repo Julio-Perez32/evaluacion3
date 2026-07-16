d2056b8ea5930a51eb4ff9aaff85892eb8602792
julio <20210261@ricaldone.edu.sv>
2026-07-16 13:28:29 -0600

    controlador de wompi terminado

--- backend/src/controller/wompiController.js
+++ backend/src/controller/wompiController.js
@@ -1,38 +1,49 @@
 import fetch from "node-fetch"
-import {config} from "../../config.js"
+import { config } from "../../config.js"
 const wompiController = {}
 
-wompiController.generateToken = async(req,res) =>{
-    try{
-        const response = await fetch("https://id-wompi.sv/token",{
+wompiController.generateToken = async (req, res) => {
+    try {
+        const response = await fetch("https://id-wompi.sv/token", {
             method: "POST",
-            headers:{
+            headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             },
             body: new URL.SearchParams({
-                grant_type:config.wompi.grant_type,
+                grant_type: config.wompi.grant_type,
                 audience: config.wompi.audience,
                 client_id: config.wompi.client_id,
                 client_secret: config.wompi.client_secret
             })
         });
-        if(!response.ok){
+        if (!response.ok) {
             const error = await response.text;
-            return res.status(500).json({error})
+            return res.status(500).json({ error })
         }
         const data = await response.json();
         return res.status(200).json(data);
-    }catch (error) {
+    } catch (error) {
         console.error("error al obtener los datos: ", error)
         return res.status(500).json({ message: "error interno nose jeje xdxdxdxd" })
     }
 }
-wompiController.paymentTest = async(req, res) =>{
-    try{
-        const {token, formData} = req.body;
-        const response = await fetch("https://wompi.sv/TransaccionCompra/TokenizadaSin3Ds")
+wompiController.paymentTest = async (req, res) => {
+    try {
+        const { token, formData } = req.body;
+        const response = await fetch("https://wompi.sv/TransaccionCompra/TokenizadaSin3Ds", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(formData)
+        })
+        if(!response.ok){
+            const error = await response.text()
+            return res.status(500).json(error)
+        }
     }catch (error) {
-        console.error("error al obtener los datos: ", error)
-        return res.status(500).json({ message: "error interno nose jeje xdxdxdxd" })
-    }
+    console.error("error al obtener los datos: ", error)
+    return res.status(500).json({ message: "error interno nose jeje xdxdxdxd" })
+}
 }
